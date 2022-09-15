ec872c8636835346ef805b29697611b444814c67
robjweiss <rweiss55@gmail.com>
2022-09-15 16:30:42 +0900

    import express

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,5 @@
+import express from 'express'
+
 export function mask(obj: object, fields: Array<string>, options?: object): void;
 
 /**
@@ -12,4 +14,4 @@ type Log = {
     userAgent: string
 }
 
-export function format(req: Express.Request, res: Express.Response, options?: object): Log;
+export function format(req: express.Request, res: express.Response, options?: object): Log;
