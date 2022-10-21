e821061bd9a85646e1ec720eefa97f8ef8317fea
Gensuke Toshiro <toshiro@openrm.co.jp>
2022-10-21 22:39:50 +0900

    Use interfaces

--- index.d.ts
+++ index.d.ts
@@ -1,26 +1,26 @@
 import http from 'http';
 import express from 'express';
 
-type MaskOptions = {
+interface MaskOptions {
     placeholder?: string;
-};
+}
 
-type LogOptions = {
+interface LogOptions {
     maxBodyLen?: number;
     mask?: MaskOptions & { fields?: string[] };
-};
+}
 
 type RequestKeys = 'ip' | 'ips' | 'hostname' | 'protocol' | 'method' | 'path' | 'fresh' | 'subdomains' | 'xhr' | 'headers' | 'trailers' | 'query' | 'params';
 type ResponseKeys = 'locals';
 
-type Log = Pick<express.Request, RequestKeys> & Pick<express.Response, ResponseKeys> & {
+interface Log extends Pick<express.Request, RequestKeys>, Pick<express.Response, ResponseKeys> {
     url: string;
     referer: string;
     userAgent: string;
     protocolVersion: typeof express.request.httpVersion;
     responseHeaders: http.OutgoingHttpHeaders;
     // TODO: annotate properties from third party libraries such as express-session, cookie-parser
-};
+}
 
 declare function ExpressLoggingJson(req: express.Request, res: express.Response, options?: LogOptions): Log;
 
